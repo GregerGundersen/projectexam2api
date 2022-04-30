9ea25dec2b69ca8d81e38523bc95e1361c35ec87
GregerGundersen <gregergundersen@gmail.com>
2022-04-30 18:01:32 +0200

    Pls

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env(
-    "postgres://jgfnpxdhqfbypg:a0545ab9f495c8ecf17e28e2661127a0b5232e00f0cb516988d213d044532bd3@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/dc03cd98gugka5"
-  ),
+  url: env(process.env.DATABASE_URL),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["yourkey1", "yourkey2"]),
+    keys: env.array("APP_KEYS"),
   },
 });
